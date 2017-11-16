c5a133cd64523d9da2149f783dea47314c31a56e
Abhinav Verma <abhinavv@gwu.edu>
2017-11-16 11:10:17 -0500

    coding test Implementation

---
+++ BillingApp/BillingApp/BillingApp.Program/CodingTestTax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BillingApp.BillingApp.Program
+{
+    public class CodingTestTax:ITax
+    {
+        float ITax.Percentage { get; set; }        
+        string ITax.Name { get; set; }        
+        HashSet<ItemTypeEnum> ITax.ExemptedItemTypes { get; set; }        
+    }
+}
